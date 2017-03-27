a3cba2d2981aed1c382a38b390de67561b746ce5
= <=>
2017-03-27 21:18:34 +0530

    Camera has been made

--- app/src/main/java/com/example/joshiyogesh/camera_bear/MainActivity.java
+++ app/src/main/java/com/example/joshiyogesh/camera_bear/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.joshiyogesh.camera_bear;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
@@ -20,15 +26,30 @@ public class MainActivity extends AppCompatActivity {
     // Activity request codes
     private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
     public static final int MEDIA_TYPE_IMAGE = 1;
-    private Uri uri; //to store image ,, uri
+    private Uri fileUri; //to store image ,, uri
 
     /*Directory Name where Image will be saved*/
     private static String IMAGE_DIRECTORY_NAME = "myDirectory";
 
+    ImageView imageView;
+    Button takeImageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        takeImageButton = (Button)findViewById(R.id.buttonClick);
+        takeImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkCameraHardware(MainActivity.this)){
+                    captureImage();
+                }else {
+                    Toast.makeText(MainActivity.this,"Device Doesn't have Camera",Toast.LENGTH_LONG)
+                            .show();
+                }
+            }
+        });
 
 
     }
@@ -47,13 +68,12 @@ public class MainActivity extends AppCompatActivity {
     /*Capturing image will launch camera app request capture*/
     private void captureImage(){
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        uri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT,uri);
-        /**/
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,fileUri);
         startActivityForResult(intent,CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
     }
     /*
-    * Helper methods
+    * Helper methods ------------------------------------------------------------
     * */
     /*
     * Creating file uri to store image
@@ -63,7 +83,8 @@ public class MainActivity extends AppCompatActivity {
     }
     private static File getOutputMediaFile(int type){
         //External SD card location
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),IMAGE_DIRECTORY_NAME);
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory
+                (Environment.DIRECTORY_PICTURES),IMAGE_DIRECTORY_NAME);
         //Create Storage Directory if it does not exist
         if (!mediaStorageDir.exists()){
             if (!mediaStorageDir.mkdirs()){
@@ -81,6 +102,61 @@ public class MainActivity extends AppCompatActivity {
         }else {return null;}
 
     return mediaFile;
+    }
+/*---------------------------------------------------------------------------------*/
+    /*
+    * Receiving Activity result will be called after Camera Has been Closed  , i.e
+    *image has been cliked
+    * */
+    @Override
+    protected void onActivityResult(int requestCode , int resultCode ,Intent data){
+//       if the result is capturing Image is Right
+        if (requestCode == CAMERA_CAPTURE_IMAGE_REQUEST_CODE){
+            if (resultCode == RESULT_OK){
+            /*call Display Image methods*/
+                preViewCaptureImage();
+            }else if (resultCode == RESULT_CANCELED){
+//                User Force to press Back Button ,, Image Doesnot Clicked
+                Toast.makeText(MainActivity.this,"User Cancelled Image Capture",Toast.LENGTH_LONG)
+                        .show();
+            }
+            else {
+                /*SomeThing BAD with Code*/
+                Toast.makeText(MainActivity.this,"Sorry ! Developer Fault",Toast.LENGTH_LONG)
+                        .show();
+            }
+        }
+    }
+
+    /*methods to display Image Which Has been Clicked By User*/
+    private void preViewCaptureImage(){
+       try {
+        /*Bitmap using for Image*/
+           BitmapFactory.Options options = new BitmapFactory.Options();
+           options.inSampleSize = 10;
+           final Bitmap bitmap = BitmapFactory.decodeFile(fileUri.getPath(), options);
+           imageView.setImageBitmap(bitmap);
+       }
+       catch (NullPointerException e){
+           e.printStackTrace();
+       }
+
+    }
+
+    /*
+    * Here we store the file url as it would be null after returning from
+    *camera app
+    * */
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putParcelable("file_uri",fileUri);
+    }
+    /*here we have to store file uri again*/
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        fileUri = savedInstanceState.getParcelable("file_uri");
     }
 
 }
